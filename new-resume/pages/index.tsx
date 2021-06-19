cda21f0575dac4c199ff54b7e95b719bf650859e
ashur-k <ashurkanwal@yahoo.com>
2021-06-19 10:03:07 +0100

    updated about page learnt about deangerously set inner html

--- new-resume/pages/index.tsx
+++ new-resume/pages/index.tsx
@@ -1,12 +1,40 @@
 import { GetServerSideProps, GetStaticPropsContext } from 'next'
 import { services } from '../data'
+import ServiceCard from '../components/ServiceCard'
 
-
-const Home = ({services}) => {
-  // console.log('service:',services)
+const Home = () => {
+ 
   return (
-    <div>
-      <h1 className="text">Hello World</h1>
+    <div className="flex flex-col flex-grow px-6 pt-1 ">
+      <h5 className="my-3 text-base font-medium">
+        Full Stack Web Developer, qualified with a Diploma 
+        in Software Development from Code Institute (CI) 
+        and software engineering degree from Pakistan 
+        institute of management science and technology (PIMSAT). 
+        Strong understanding of object-oriented programming and 18 
+        months experience of web-development. I am developing my 
+        skills set to be a great asset for my employer.  
+      </h5>
+      <div
+        className="flex-grow p-4 mt-5 bg-gray-400 dark:bg-dark-100 "
+        style={{ marginLeft: "-1.5rem", marginRight: "-1.5rem" }}
+      >
+        <h4 className="my-3 text-xl font-semibold tracking-wide">
+          What I am doing
+        </h4>
+        <div className="grid gap-6 my-3 md:grid-cols-2">
+            {
+              services.map(service=> (
+                <div 
+                  className="col-span-2 p-2 bg-gray-200 rounded-lg dark:bg-dark-200 md:col-span-1 "
+                  key={service.title} 
+                >
+                  <ServiceCard service={service} />
+                </div>
+              ))
+            }
+        </div>
+      </div>
     </div>
   )
 }
@@ -25,18 +53,18 @@ export default Home
 //   }
 // } 
 
-export const getStaticProps = async (
+// export const getStaticProps = async (
   
-  context:GetStaticPropsContext
+//   context:GetStaticPropsContext
   
-  ) => {
+//   ) => {
   
-  const res = await fetch('http://localhost:3000/api/services')
-  const data = await res.json()
+//   const res = await fetch('http://localhost:3000/api/services')
+//   const data = await res.json()
 
-  return {
-    props:{
-      services: data.services,
-    }
-  }
-} 
+//   return {
+//     props:{
+//       services: data.services,
+//     }
+//   }
+// } 
