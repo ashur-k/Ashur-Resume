7d860c720d31ee18207721c61f0b7daa5d55fb5d
ashur-k <ashurkanwal@yahoo.com>
2021-07-02 14:58:16 +0100

    updated head in app.js

--- new-resume/pages/_app.tsx
+++ new-resume/pages/_app.tsx
@@ -1,7 +1,7 @@
 import '../styles/globals.css'
 import Sidebar from '../components/Sidebar'
 import Navbar from '../components/Navbar'
-
+import Head from 'next/head';
 import { ThemeProvider } from 'next-themes'
 import { AnimatePresence } from 'framer-motion'
 
@@ -9,6 +9,10 @@ function MyApp({ Component, pageProps, router }) {
   
   return (
     <ThemeProvider attribute="class">
+      <Head>
+        <title>Ashur Kanwal</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <div className="grid grid-cols-12 gap-6 px-5 my-14 lg:mb-0 md:mb-16 sm:px-20 md:px-32 lg:px-36 xl:px-48 ">
         <div className="h-full col-span-12 p-4 text-base text-center bg-white dark:bg-dark-500 lg:col-span-3 rounded-2xl shadow-custom-light dark:shadow-custom-dark ">
           <Sidebar />
--- new-resume/pages/_document.tsx
+++ new-resume/pages/_document.tsx
@@ -10,7 +10,6 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Kaushan+Script&display=swap"
             rel="stylesheet"
           />
-         
         </Head>
         <body className="bg-fixed bg-gradient-to-r from-green to-blue-500 dark:from-dark-500 dark:to-dark-700 dark:text-white">
           <Main />
